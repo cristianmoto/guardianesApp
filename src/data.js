8cb29ee8d42f4fa3a1693d37cb584173f45fc1a8
Cristian Aguirre <cristianmoto066@gmail.com>
2023-05-01 09:09:25 -0300

    repositorio con los personajes

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export default[
+      {
+        id: "Peter Quill",
+        alias: "Star-Lord",
+        especie: "Humano",
+        edad: 39,
+        img: 'https://ibb.co/ZSLJhgw',
+        peliculas: 5,
+        habilidades: "Experto en el uso de armas de fuego",
+        
+      },
+      {
+        id: "Gamora",
+        alias: null,
+        especia: "Zen-Whoberi",
+         edad: 33,
+        img: 'https://ibb.co/fpCvFrz ',
+        peliculas: 3,
+        habilidades: "Experta en combate cuerpo a cuerpo",
+         
+      },
+      {
+        id: "Drax",
+        alias: "El Destructor",
+        especie: "Desconocida",
+        edad: 6,
+        img: 'https://ibb.co/XZNkTRc',
+        peliculas: 3,
+        habilidades: "Fuerza y resistencia sobrehumanas",
+         
+      },
+      {
+        id: "Rocket",
+        alias: null,
+        especie: "Mapache",
+        edad: 5,
+        img: 'https://ibb.co/0jFm1cX ',
+        peliculas: 5,
+        habilidades: "Experto en armas y tecnología",
+         
+      },
+      {
+        id: "Groot",
+        alias: null,
+        especie: "Flora colossus",
+        edad: 12,
+        img: 'https://ibb.co/jWB9Dgm',
+        peliculas: 3,
+        habilidades: "Regeneración acelerada",
+         
+      }
+    ]
+ 
+  
